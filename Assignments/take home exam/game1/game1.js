863f65f92e2ade586556714a7ef56769007ca783
ashokja <ashok.j13@iiits.in>
2016-01-01 22:25:40 +0530

    games updated

--- Assignments/take home exam/game1/game1.js
+++ Assignments/take home exam/game1/game1.js
@@ -1,11 +1,7 @@
 var o=['अ','आ','इ','ई','उ','ऊ','ए','ऐ','ओ','औ','क','ख','ग','घ','ङ','च','छ','ज','झ','ञ','ट','ठ','ड','ढ','ण','त','थ','द','ध','न','प','फ','ब','भ','म','य','र','ल','व','श','ष','स','ह','ा','ि','ी','ु','ू','ृ',' ॄ','ॅ','े','ै','ॉ','ो','ौ'];
-//var main=['हफ्ता','साल','आज','कल','कैलेंडर','सेकंड','घंटा','मिनट','बजे','घड़ी','सकना','करना','जाना','आना','हसना','बनाना','देखना','दूर','छोटा','सुंदर','बदसूरत','कठिन','अच्छा','आदमी','आप','आशा','और','एक','कमरा','कुरसी','खिड़की','गाड़ी','गुजराती','चाचा','चिड़िया','छोटा','जर्मन','जापानी','ठीक','तीन','तुम','तू','दादी','दूसरा','दो','नहीं','पंखा','पिता','पुराना','बड़ा','बहुत','बहू','बूढ़ा','भारतीय','मकान','माता','मानव','यह','रविवार','राजा','लाल','लोग','वह','हम','हवादार','है','अभी','आज','और','कितना','किताब']
-//var main=['एक',' दो','तीन','चार','पांच','छह','सात','आठ','नौ','दस','ग्यारह','बारह','तेरह','चौदह','पंद्रह','सोलह','सत्रह','अठारह','उन्नीस','बीस'];
 var main = ['बाधाम','चौलाई','सेब','बेल','केले','काली बेरी','लौकी','बैंगन','पत्ता गोबी',' गाजर','शिताफल','अंगूर','अमरूद','कटहल','जामुन','आम']
 
 var telugu = ['అ','ఆ','ఇ','ఈ','ఉ','ఊ','ఋ','ఎ','ఏ','ఐ','ఒ','ఓ','ఖ','గ','ఘ','ఙ','చ','ఛ','జ','ఞ','ట','ఠ','డ','ఢ','ణ','త','థ','ద','ధ','న','ప','ఫ','బ','భ','మ','య','ర','ఱ','ల','ళ','వ','శ','ష','స','హ'];
-//var telugu_dict = ['వారం','ఇయర్','నేడు','రేపు','నిజమె','కాదు','ఏంటి','ఆంగ్లము','ఎల','ఎక్కడ','ఎప్పుడు','ఎవరు','ఏది','ఎంత','ఎన్ని','వద్దు','లేదు','కాదు','రాదు','బాలేదు','తెలియదు','నాకు','కాలేదు','భాష','నాపేరు','ఉంది','ఎలా','ఉన్నారు','ఎక్కడికి','ఏంటి','ఎక్కడ','ఎందుకు','ఎవరు','ఎలా','ఏమి','ఉంది','లేదు','నాకు','కావాలి','మాసం','సంవత్సరం','మాసం','తేది','నూతన','ఇది','చాలా','బాగుంది','మీరు','ఎక్కడ','నను','సినిమా','చూస్తున్నాను','ఉంది','తరువు','తరవండి','అవును','నాకోసం','మీకోసం','నకోసం','కాదు','అవును','ఎంత','కాలేదు','నాకు','సున్న','ఒక','రెండు','మూడు','నాలుగు','ఐదు','ఆరు','ఏడు','ఎనిమిది','తొమ్మిది','పది','పదకొందు','పన్నెండు','పదమూడు','పద్నాలుగు','పదిహేను','పదహారు','పదిహేడు','పద్దెనిమిది','పంతొమ్మిది','ఇరవై','ఇరవైఒకటి','ఇరవైరెండు','ఇరవైమూడు','నలబై','వంద' ,'వెయ్యి','రెండువేలు','కోటి','సగం','ముందు','ఉదయం','రాత్రి','ఒక','గంటలు','ఒక్క','క్షణం','నిమిషాలు','గంటలు','రోజు','వారం','నెల','రోజు' ,'నేడు','నేడు','రేపు','ఈవారం','గత','వారం','వారం','ఆది','సోమ','మంగళ','బుధ','గురు','శుక్ర','శని','మాఘము','ఆషాఢము','రంగు','నలుపు'];
-//var telugu_dict =['ఒక్కటి','రెండు','మూడు','నాలుగు','ఐదు','ఆరు','ఏడు','ఎనిమిది','తొమ్మిది','పది','పదకొండు','పన్నెండు','పదమూడు ','పధ్నాలుగు','పదిహేను','పదహారు','పదిహేడు','పద్దెనిమిది','పందొమ్మిది','ఇరవై'];
 var telugu_dict =['బాధం','అమర్నాథ్','ఆపిల్','మేడి పండు','అరటి పండు','నల్ల రేగు పండు','సొరకాయ','వంకాయ','క్యాబేజీ','క్యారెట్',' సీత ఫలం','ద్రాక్ష','జామ పండు','పనస పండు','నేరేడి పండు','మామిడి']
 var getElementId=[11,12,13,14,15,21,22,23,24,25,31,32,33,34,35,41,42,43,44,45,51,52,53,54,55];
 var mainarray=o[0];
@@ -78,7 +74,7 @@ for(l=1,v=45;l<=5;l++,v++)
 		document.getElementById("10"+l).innerHTML = o[v]; 
 	
 }
-for(l=1,V=45;l<=5;l++,V++)
+for(l=1,V=50;l<=5;l++,V++)
 {
 		document.getElementById("11"+l).innerHTML = o[V]; 
 	
@@ -86,42 +82,7 @@ for(l=1,V=45;l<=5;l++,V++)
 document.getElementById("121").innerHTML=o[55];
 }
 
-
-
-/*function setlanguage()							//Loads the alphabets in the table as the user selects the language option
-{
-	//score = 0;
-	//var t = 11;
-	var h=0;
-	var lang = document.getElementById('Language');							// Returns the language selected by the user
-	var langval = lang.options[lang.selectedIndex].value;
-	console.log('value is ' + langval);
-	for(t;t<=55;t++)
-	{
-		var get = document.getElementById(t);
-		
-		if (langval == 'telugu')												// For telugu, generate random table
-		{
-for(var z=0;z<telugu.length; z++)
-			var rand = telugu[z];
-
-	document.getElementById["11"].innerHTML=rand;
-		}
-		
-		if (langval == 'hindi')													// For Hindi, generate random table
-		{
-			for(var z=0; z<o.lenghth;o++)
-			var rand = o[k];
-			k++;
-		}
-		document.getElementById(t).innerHTML=rand;
-
-		if (t%5 == 0)
-			t = t+5;
-	}
-	word='';
-}*/
-function getdata(a,b)															// Retrieve the data from the cell clicked
+function getdata(a,b)															
 {
 	a=a-1;
 	b=b-1;
@@ -133,28 +94,16 @@ function getdata(a,b)															// Retrieve the data from the cell clicked
 }
 function Check()																// Check whether entered word is valid word or not.
 {
-	//if(word=='')
-	//	alert("Select atleast 1 letter");
+
 	var flag=0;
 	console.log(word);
-	//var lang = document.getElementById('Language');
-	//var langval = lang.options[lang.selectedIndex].value;
-	//if(langval=='hindi')														// Check for Hindi word whether its equal or not
-	//{
 	for(var k=0;k<main.length;k++)
 	{
 		console.log(main[k]);
 		if(word==main[k])
 		{
 		flag=1;
-		//var trans=telugu_dict[k];
-		//document.getElementById("trans").innerHTML="Congratulations!! " + "Your " + "score " + "is " + trans;
-
 document.write(telugu_dict[k]);
-		//score=score+2;
-
-		//document.getElementById("Score").innerHTML="Congratulations!! " + "Your " + "score " + "is " + score;		// Word found
-//function print(k);
 		break;
 		}
 	}
@@ -162,27 +111,3 @@ document.write(telugu_dict[k]);
 		document.getElementById("Score").innerHTML="Sorry!! " + "Word " + "not " + "found.";		// Word is not valid
 	flag=0;
 }
-	//}
-	//if(langval=='telugu')
-	//{																			// Check for Telugu word whether its equal or not
-	/*for(var k=0;k<telugu_dict.length;k++)
-	{
-		console.log(main[k]);
-		if(word==telugu_dict[k])
-		{
-		flag=1;
-		//score=score+2;
-		//document.getElementById("Score").innerHTML="Congratulations!! " + "Your " + "score " + "is " + score;	//Word found
-		var y=main[k];
-		document.getElementbyid("y").innerhtml="your " + "word " + y;
-		break;
-		}
-	}
-	if(flag==0)
-		document.getElementById("Score").innerHTML="Sorry!! " + "Word " + "not " + "found.";			//Word not valid
-	flag=0;
-	//}
-	word='';
-	document.getElementById("Selectedletter").innerHTML='';
-}*/
-
